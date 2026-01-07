4cf5b72dd8137669a63fffe9b30530e6bc24ba6b
Ayush Uttarwar <111568550+Ayush086@users.noreply.github.com>
2026-01-07 10:27:28 +0000

    server created

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mcp-server
+# MCP Server
 
 ## Setup Guide
 ```sh
@@ -12,3 +12,26 @@ uv init .
 ```python
  uv add fastmcp
  ```
+
+ #### AFter completing the setup
+ **to run the server**
+ ```
+ fastmcp run main.py --transport http --host 0.0.0.0 --port 8000
+ ```
+
+ **OR**
+
+ ```
+uv run main.py
+ ```
+
+
+#### Test the server in mcp inspector
+```
+uv run fastmcp dev main.py
+```
+- In left sidebar select the transport type to *streamable HTTP*
+- Check the URL and click on *connect*
+- Now we can inspect the mcp server from navigation bar
+- *resources* tab can be used to see the resources, after clicking on *list resources*
+- In *tool* we will be able to access the tools and use then in side window
--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from mcp-server!")
+from fastmcp import FastMCP
+import random
+import json
+
+mcp = FastMCP('Simple Calculator Server')
+
+# tool: add two numbers
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """ Add two numbers """
+    return a+b
+
+# tool: generate a random number
+@mcp.tool
+def random_number_generator(min_val: int = 1, max_val: int = 100) -> int:
+    """Generate a random number within a range. """
+    return random.randint(min_val, max_val)
+
+# resource: server information
+@mcp.resource('info://server')
+def server_info() -> str:
+    """ Get information about this server. """
+    infor = {
+        "name": "Simple Calculator server",
+        "version": "1.0.0",
+        "description": "A basic MCP server with math tools",
+        "tools": ["add", "random_number"],
+        "author": "coder"
+    }
+    return json.dumps(info, indent=2)
+
 
 
 if __name__ == "__main__":
-    main()
+    # in local
+    # mcp.run() - default, making it run on same machine
+    mcp.run(transport="http", host='0.0.0.0', port=8000)
